6d1df0923cc9d9a6ef1cf86eab09a4f8a8daa76f
prabin544 <42320486+prabin544@users.noreply.github.com>
2021-05-24 13:37:53 -0500

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 // oh no, you've done the security flaw and exposed your DB password here. Literally anyone could use this to log in and drop your entire database.
 // this is why we use .env files that are .gitignored.
 module.exports={
-    mongoURI:"mongodb+srv://prabin544:PKEubmQBW99h9ueh@cluster0.nqbju.mongodb.net/covid?retryWrites=true&w=majority",
+    mongoURI:"MONGO URI goes here",
 }
